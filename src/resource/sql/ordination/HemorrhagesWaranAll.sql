503cbf7fb63e243b5ef321b7dde1e311a589fde6
IvoKV <ivo.kristensjo-vukmanovic@sll.se>
2022-03-04 15:16:07 +0100

    Added option text values for hemorrhages

--- src/resource/sql/ordination/HemorrhagesWaranAll.sql
+++ src/resource/sql/ordination/HemorrhagesWaranAll.sql
@@ -7,73 +7,73 @@ SELECT  c.id, p.pid, p.SSN, p.SSN_TYPE, rp.FIRSTNAME, rp.LASTNAME,
 
         /* Hemorrhages (blödningar) */
         CASE herr.h1
-            WHEN 1 THEN 'test'
-            WHEN 2 THEN 'test'
-            WHEN 3 THEN 'test'
-            or ''
+            WHEN 1 THEN 'Nej'
+            WHEN 2 THEN 'Ja'
+            WHEN 0 THEN 'Vet ej'
+            ELSE ''
             END  AS 'Lever_eller_njursjukdom',
 
         CASE herr.e1
-            WHEN 1 THEN 'test'
-            WHEN 2 THEN 'test'
-            WHEN 3 THEN 'test'
-            OR ''
+            WHEN 1 THEN 'Nej'
+            WHEN 2 THEN 'Ja'
+            WHEN 0 THEN 'Vet ej'
+            ELSE ''
             END AS 'Etanolmissburk',
 
         CASE herr.m
-            WHEN 1 THEN 'test'
-            WHEN 2 THEN 'test'
-            WHEN 3 THEN 'test'
-            OR ''
+            WHEN 1 THEN 'Nej'
+            WHEN 2 THEN 'Ja'
+            WHEN 0 THEN 'Vet ej'
+            ELSE ''
             END AS 'Malignitet',
 
         CASE herr.r1
-            WHEN 1 THEN 'test'
-            WHEN 2 THEN 'test'
-            WHEN 3 THEN 'test'
-            OR ''
+            WHEN 1 THEN 'Nej'
+            WHEN 2 THEN 'Ja'
+            WHEN 0 THEN 'Vet ej'
+            ELSE ''
             END AS 'Reducerat trombocytantal/funktion',
 
         CASE herr.r2
-            WHEN 1 THEN 'test'
-            WHEN 2 THEN 'test'
-            WHEN 3 THEN 'test'
-            OR ''
+            WHEN 1 THEN 'Nej'
+            WHEN 2 THEN 'Ja'
+            WHEN 0 THEN 'Vet ej'
+            ELSE ''
             END AS 'Tidigare blödning',
 
         CASE herr.h2
-           WHEN 1 THEN 'test'
-           WHEN 2 THEN 'test'
-           WHEN 3 THEN 'test'
-           OR ''
-           END AS 'Hypertoni',
+            WHEN 1 THEN 'Nej'
+            WHEN 2 THEN 'Ja'
+            WHEN 0 THEN 'Vet ej'
+            ELSE ''
+            END AS 'Hypertoni',
 
         CASE herr.a
-            WHEN 1 THEN 'test'
-            WHEN 2 THEN 'test'
-            WHEN 3 THEN 'test'
-            OR ''
+            WHEN 1 THEN 'Nej'
+            WHEN 2 THEN 'Ja'
+            WHEN 0 THEN 'Vet ej'
+            ELSE ''
             END AS 'Anemi',
 
         CASE herr.g
-            WHEN 1 THEN 'test'
-            WHEN 2 THEN 'test'
-            WHEN 3 THEN 'test'
-            OR ''
+            WHEN 1 THEN 'Nej'
+            WHEN 2 THEN 'Ja'
+            WHEN 0 THEN 'Vet ej'
+            ELSE ''
             END AS 'Genetiska faktorer (CYP2C9, VKORC1)',
 
        CASE herr.e2
-           WHEN 1 THEN 'test'
-           WHEN 2 THEN 'test'
-           WHEN 3 THEN 'test'
-           OR ''
+           WHEN 1 THEN 'Nej'
+           WHEN 2 THEN 'Ja'
+           WHEN 0 THEN 'Vet ej'
+           ELSE ''
            END AS 'Stor risk för fall',
 
        CASE herr.s
-           WHEN 1 THEN 'test'
-           WHEN 2 THEN 'test'
-           WHEN 3 THEN 'test'
-           OR ''
+           WHEN 1 THEN 'Nej'
+           WHEN 2 THEN 'Ja'
+           WHEN 0 THEN 'Vet ej'
+           ELSE ''
            END AS Stroke
 
 FROM centre as c
