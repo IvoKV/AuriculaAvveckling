1544071dd3e8a21fa08ae96951fd497949fab4a0
IvoKV <ivo.kristensjo-vukmanovic@sll.se>
2022-04-29 15:25:15 +0200

    work day 04-29: pdfbox finished for Befattningar

--- src/main/java/PDF/PDFPersonInChargeTioHundra.java
+++ src/main/java/PDF/PDFPersonInChargeTioHundra.java
@@ -1,6 +1,5 @@
 package PDF;
 
-
 import Person.PersonInCharge;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -12,13 +11,12 @@ import java.io.IOException;
 import java.util.List;
 
 public class PDFPersonInChargeTioHundra {
-
     List<PersonInCharge> personsInCharge;
     private final String pdfPathFileName = "PDFPersonInChargeTioHundra.pdf";
 
     private float x = 0;
     private float y = 750;
-    private float leading = 20;
+    private final float leading = 20;
     private float fontSize = 12;
     private float fontHeight = fontSize;
 
@@ -31,25 +29,26 @@ public class PDFPersonInChargeTioHundra {
     private PDDocument document = null;
     private PDPage page = null;
     private PDPageContentStream contentStream = null;
-
+    private int sidaNo = 0;
 
     public PDFPersonInChargeTioHundra(List<PersonInCharge> personsInCharge) throws IOException {
         this.personsInCharge = personsInCharge;
+
+        /** Initialize document and first page **/
         this.document = new PDDocument();
         this.page = new PDPage();
-        writeHeader();
+
+        /** Initialize content stream, first page **/
+        contentStream = new PDPageContentStream(document, page);
     }
 
     private void writeHeader() throws IOException {
-        document.addPage(page);
-        contentStream = new PDPageContentStream(document, page);
-        contentStream.setFont(fontBold, fontSize + 2);
-
         float yCordinate = page.getCropBox().getUpperRightY() - 30;
         float startX = page.getCropBox().getLowerLeftX() + 30;
         float endX = page.getCropBox().getUpperRightX() - 30;
 
         contentStream.beginText();
+        contentStream.setFont(PDType1Font.COURIER_BOLD, 14f);
         contentStream.newLineAtOffset( startX, yCordinate);
         contentStream.showText("Befattningar hos vårdgivaren TIO 100");
         yCordinate -= fontHeight;
@@ -59,28 +58,92 @@ public class PDFPersonInChargeTioHundra {
         contentStream.lineTo(endX, yCordinate);
         contentStream.stroke();
         yCordinate -= leading;
+        y = yCordinate;
+    }
 
-        contentStream.close();
+    public void createPersonInChargePDFDetails() throws IOException {
+        if(contentStream != null) {
+            float startX = page.getCropBox().getLowerLeftX() + 30;
+            float tabSpaceX = 100;
+
+            /** First page **/
+            document.addPage(page);
+            contentStream = new PDPageContentStream(document, page);
+            contentStream.setFont(fontBold, fontSize + 2);
+            writeHeader();
+
+            /** SKRIV UT BEFATTNINGAR (LISTA) **/
+            personsInCharge.stream().forEach(item -> {
+                try {
+                    /* FÖRNAMN */
+                    contentStream.beginText();
+                    contentStream.setFont(PDType1Font.COURIER, 12);
+                    contentStream.newLineAtOffset( startX, y);
+                    contentStream.showText("Förnamn:");
+                    contentStream.newLineAtOffset( tabSpaceX, 0);
+                    contentStream.showText(item.getFirstName());
+                    contentStream.endText();
+                    y -= leading;
+
+                    /* EFTERNAMN */
+                    contentStream.beginText();
+                    contentStream.newLineAtOffset( startX, y);
+                    contentStream.showText("Efternamn:");
+                    contentStream.newLineAtOffset( tabSpaceX, 0);
+                    contentStream.showText(item.getLastName());
+                    contentStream.endText();
+                    y -= leading;
+
+                    /* BEFATTNING */
+                    contentStream.beginText();
+                    contentStream.newLineAtOffset( startX, y);
+                    contentStream.showText("Befattning:");
+                    contentStream.newLineAtOffset( tabSpaceX, 0);
+                    contentStream.showText(item.getBefattningskod());
+                    contentStream.endText();
+                    y -= leading * 2;
+
+                    if(y <= leading * 3){
+                        contentStream.close();
+                        this.page = new PDPage();
+                        document.addPage(page);
+                        contentStream = new PDPageContentStream(document, page);
+                        writeHeader();
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            contentStream.close();
+            writePageNumbers();
+        }
         document.save(pdfPathFileName);
         document.close();
     }
 
-    public void createPersonInChargePDFDetails() throws IOException {
+    private void writePageNumbers() throws IOException {
+        float yCordinate = page.getCropBox().getUpperRightY() - 30;
+        float endX = page.getCropBox().getUpperRightX() - 100;
 
-        //contentStream = new PDPageContentStream(document, page);
-        if(contentStream != null) {
+        int pagecounter = 1;
+        int sumPages = document.getPages().getCount();
+
+        for(PDPage page : document.getPages()){
+            StringBuilder sb = new StringBuilder();
+            sb.append("sid: ");
+            sb.append(pagecounter);
+            sb.append(" (" + sumPages + ")");
+
+            PDPageContentStream contentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, true, false);
             contentStream.setFont(PDType1Font.COURIER, 12);
-            y -= 24;
-            contentStream.beginText();
-            contentStream.newLineAtOffset(x, y);
 
-            // todo: fortsätt härifrån
-            personsInCharge.get(0).getBefattningskod();
-            contentStream.showText("Hello World!");
+            contentStream.beginText();
+            contentStream.newLineAtOffset(endX, yCordinate);
+            contentStream.showText(sb.toString());
             contentStream.endText();
             contentStream.close();
+            pagecounter++;
         }
-        //document.save(savePDFPathName);
-        document.close();
     }
 }
